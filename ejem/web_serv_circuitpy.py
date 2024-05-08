70e5d9ab906a7521532dd704df4a4f2a026e8a85
Gerardo Munoz <36160037+GerardoMunoz@users.noreply.github.com>
2024-05-08 17:05:39 -0500

    Add files via upload

---
+++ ejem/web_serv_circuitpy.py
@@ -0,0 +1,21 @@
+import socketpool
+import wifi
+
+wifi.radio.connect("Ejemplo","12345678")
+pool=socketpool.SocketPool(wifi.radio)
+
+print("wifi.rado",wifi.radio.hostname, wifi.radio.ipv4_address)
+s = pool.socket()
+s.bind(('', 80))
+s.listen(5)
+
+while True:
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  buffer = bytearray(1024)  # Create a mutable buffer
+  bytes_received, address = conn.recvfrom_into(buffer)  # Receive data into the buffer and get the sender's address
+  print("Received from:", address)
+  print("Received data:", buffer[:bytes_received])  # P
+  response = "Hola"
+  conn.send(response)
+  conn.close()
