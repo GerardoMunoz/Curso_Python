a7357a1c2c6efc0787dadc25c9981b9e412cf638
Gerardo Munoz <36160037+GerardoMunoz@users.noreply.github.com>
2024-08-24 12:22:56 -0500

    Update web_serv_circuitpy.py

--- ejem/web_serv_circuitpy.py
+++ ejem/web_serv_circuitpy.py
@@ -4,8 +4,9 @@ import wifi
 wifi.radio.connect("Ejemplo","12345678")
 pool=socketpool.SocketPool(wifi.radio)
 
-print("wifi.rado",wifi.radio.hostname, wifi.radio.ipv4_address)
+print("wifi.radio",wifi.radio.hostname, wifi.radio.ipv4_address)
 s = pool.socket()
+s.setsockopt(pool.SOL_SOCKET, pool.SO_REUSEADDR, 1)
 s.bind(('', 80))
 s.listen(5)
 
@@ -16,6 +17,17 @@ while True:
   bytes_received, address = conn.recvfrom_into(buffer)  # Receive data into the buffer and get the sender's address
   print("Received from:", address)
   print("Received data:", buffer[:bytes_received])  # P
-  response = "Hola"
+  response = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+</body>
+</html>
+
+"""
   conn.send(response)
   conn.close()
